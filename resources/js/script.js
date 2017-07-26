5ec8547518cae8eaafa2a98869fd7b4a5a848fb8
Carlos Rivera <Ossitooh@user.noreply.github.com>
2017-07-25 23:17:17 -0400

    My First Website
    This is Atribute pages for the man who make me do better in school and better in life.

---
+++ resources/js/script.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+    /* Animation */
+    $('.js--wp-1').waypoint(function(directon) {
+        $('.js--wp-1').addClass('animated zoomIn');
+    }, {
+        offset: '50%'
+    });
+    
+    
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeIn');
+    }, {
+        offset:'60%'
+    });
+    
+    /* Animation li */
+    $('.js--wp-li-1').waypoint(function(direction) {
+        $('.js--wp-li-1').addClass('animated fadeIn');
+    }, {
+        offset: '90%'
+    });
+    
+    $('.js--wp-li-2').waypoint(function(direction) {
+        $('.js--wp-li-2').addClass('animated fadeIn');
+    }, {
+        offset: '90%'
+    });
+    
+     $('.js--wp-li-3').waypoint(function(direction) {
+        $('.js--wp-li-3').addClass('animated fadeIn');
+    }, {
+        offset: '90%'
+    });
+    
+     $('.js--wp-li-4').waypoint(function(direction) {
+        $('.js--wp-li-4').addClass('animated fadeIn');
+    }, {
+        offset: '90%'
+    });
+    
+     $('.js--wp-li-5').waypoint(function(direction) {
+        $('.js--wp-li-5').addClass('animated fadeIn');
+    }, {
+        offset: '90%'
+    });
+    
+     $('.js--wp-li-6').waypoint(function(direction) {
+        $('.js--wp-li-6').addClass('animated fadeIn');
+    }, {
+        offset: '90%'
+    });
+    
+     $('.js--wp-li-7').waypoint(function(direction) {
+        $('.js--wp-li-7').addClass('animated fadeIn');
+    }, {
+        offset: '90%'
+    });
+    
+     $('.js--wp-li-8').waypoint(function(direction) {
+        $('.js--wp-li-8').addClass('animated fadeIn');
+    }, {
+        offset: '90%'
+    });
+    
+});
